f4a0bf001b02ccd4932cb0387c1df6acec2fbdbc
Uxugin <git@uxugin.com>
2024-01-28 00:41:57 -0700

    run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,7 @@ pub struct PIDController {
     int_error: f32,
 }
 impl PIDController {
-    pub fn new(setpoint: f32,
-               kp: f32,
-               ki: f32,
-               kd: f32) -> PIDController {
+    pub fn new(setpoint: f32, kp: f32, ki: f32, kd: f32) -> PIDController {
         PIDController {
             setpoint: setpoint,
             kp: kp,
@@ -19,27 +16,27 @@ impl PIDController {
             kd: kd,
             last_update_time: None,
             prev_error: None,
-            int_error: 0.0
+            int_error: 0.0,
         }
     }
     #[must_use]
     pub fn update(&mut self, time: f32, process: f32) -> f32 {
-        let error = self.setpoint-process;
+        let error = self.setpoint - process;
         let delta_time = match self.last_update_time {
             None => 0.0,
-            Some(x) => time-x
+            Some(x) => time - x,
         };
         let drv_error = match self.prev_error {
             None => 0.0,
-            Some(x) => (error-x)/delta_time
+            Some(x) => (error - x) / delta_time,
         };
         self.int_error += match self.prev_error {
-            Some(x) => delta_time*(x+error)/2.0,
+            Some(x) => delta_time * (x + error) / 2.0,
             None => 0.0,
         };
         self.last_update_time = Some(time);
         self.prev_error = Some(error);
-        self.kp*error+self.ki*self.int_error+self.kd*drv_error
+        self.kp * error + self.ki * self.int_error + self.kd * drv_error
     }
 }
 #[cfg(test)]
