3d60fdaa2fb7580b60ac464561ea5a6ed139a480
Uxugin <git@uxugin.com>
2024-01-28 00:40:15 -0700

    switch away from functions

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,12 @@ pub struct PIDController {
     prev_error: Option<f32>,
     int_error: f32,
 }
-impl PIDController{
+impl PIDController {
     pub fn new(setpoint: f32,
                kp: f32,
                ki: f32,
-               kd: f32)->PIDController{
-        PIDController{
+               kd: f32) -> PIDController {
+        PIDController {
             setpoint: setpoint,
             kp: kp,
             ki: ki,
@@ -22,12 +22,24 @@ impl PIDController{
             int_error: 0.0
         }
     }
-    pub fn update(&mut self, time: f32, process: f32){
+    #[must_use]
+    pub fn update(&mut self, time: f32, process: f32) -> f32 {
         let error = self.setpoint-process;
-        let delta_time = time-self.last_update_time;
-        let drv_error = (error-self.prev_error)/delta_time;
-        self.int_error += delta_time*(self.prev_error+error)/2.0;
-        (self.set_control)(self.kp*error+self.ki*self.int_error+self.kd*drv_error);
+        let delta_time = match self.last_update_time {
+            None => 0.0,
+            Some(x) => time-x
+        };
+        let drv_error = match self.prev_error {
+            None => 0.0,
+            Some(x) => (error-x)/delta_time
+        };
+        self.int_error += match self.prev_error {
+            Some(x) => delta_time*(x+error)/2.0,
+            None => 0.0,
+        };
+        self.last_update_time = Some(time);
+        self.prev_error = Some(error);
+        self.kp*error+self.ki*self.int_error+self.kd*drv_error
     }
 }
 #[cfg(test)]
@@ -36,5 +48,30 @@ mod tests {
 
     #[test]
     fn new() {
+        let pid = PIDController::new(5.0, 1.0, 0.01, 0.1);
+        assert_eq!(pid.setpoint, 5.0);
+        assert_eq!(pid.kp, 1.0);
+        assert_eq!(pid.ki, 0.01);
+        assert_eq!(pid.kd, 0.1);
+        assert_eq!(pid.last_update_time, None);
+        assert_eq!(pid.prev_error, None);
+        assert_eq!(pid.int_error, 0.0);
+    }
+    #[test]
+    fn first_update() {
+        let mut pid = PIDController::new(5.0, 1.0, 0.01, 0.1);
+        let new_control = pid.update(1.0, 0.0);
+        assert_eq!(new_control, 5.0);
+        assert_eq!(pid.last_update_time, Some(1.0));
+        assert_eq!(pid.prev_error, Some(5.0));
+        assert_eq!(pid.int_error, 0.0);
+    }
+    #[test]
+    fn subsequent_update() {
+        let mut pid = PIDController::new(5.0, 1.0, 0.01, 0.1);
+        let _ = pid.update(1.0, 0.0);
+        let new_control = pid.update(3.0, 1.0);
+        assert_eq!(new_control, 4.04);
+        assert_eq!(pid.int_error, 9.0);
     }
 }
