71d1b5ecd0d2ee82aa29372a7ce12a74637ae086
Uxugin <git@uxugin.com>
2024-01-27 23:59:13 -0700

    remove old stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,29 @@
 pub struct PIDController {
     setpoint: f32,
-    get_process: fn()->f32,
-    get_time: fn()->f32,
-    set_control: fn(f32),
     kp: f32,
     ki: f32,
     kd: f32,
-    last_update_time: f32,
-    prev_error: f32,
+    last_update_time: Option<f32>,
+    prev_error: Option<f32>,
     int_error: f32,
 }
 impl PIDController{
     pub fn new(setpoint: f32,
-               get_process: fn()->f32,
-               get_time: fn()->f32,
-               set_control: fn(f32),
                kp: f32,
                ki: f32,
                kd: f32)->PIDController{
         PIDController{
             setpoint: setpoint,
-            get_process: get_process,
-            get_time: get_time,
-            set_control: set_control,
             kp: kp,
             ki: ki,
             kd: kd,
-            last_update_time: get_time(),
-            prev_error: (setpoint-get_process())/setpoint,
+            last_update_time: None,
+            prev_error: None,
             int_error: 0.0
         }
     }
-    pub fn update(&mut self){
-        let value = (self.get_process)();
-        let time = (self.get_time)();
-        let error = (self.setpoint-value)/self.setpoint;
+    pub fn update(&mut self, time: f32, process: f32){
+        let error = self.setpoint-process;
         let delta_time = time-self.last_update_time;
         let drv_error = (error-self.prev_error)/delta_time;
         self.int_error += delta_time*(self.prev_error+error)/2.0;
@@ -45,33 +34,7 @@ impl PIDController{
 mod tests {
     use super::*;
 
-    /*#[test]
-    fn new() {
-        fn get_process()->f32{
-            1.0
-        }
-        fn get_time()->f32{
-            0.0
-        }
-        fn set_control(_: f32){}
-        let pid = PIDController::new(1.0, get_process, get_time, set_control, 1.0, 0.0, 0.0);
-    }*/
     #[test]
-    fn update(){
-        let mut velocity = 0f32;
-        let mut position = 0f32;
-        let mut time = 0f32;
-        fn get_process()->f32{
-            position
-        }
-        fn get_time()->f32{
-            time
-        }
-        fn set_control(value: f32){
-            time+=1;
-            velocity+=value;
-            position+=velocity;
-        }
-        let mut pid = PIDController::new(1f32, get_process, get_time, set_control, 1.0, 0.0, 0.0);
+    fn new() {
     }
 }
