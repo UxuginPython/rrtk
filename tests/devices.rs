4df4f9fb06110c4ed66b911138a5576c32e25d94
Uxugin <git@uxugin.com>
2024-03-22 02:48:14 -0600

    remove some useless stuff and add NonFeedbackMotor test

--- tests/devices.rs
+++ tests/devices.rs
@@ -227,18 +227,12 @@ fn servo_motor() {
     struct DummyServoMotor {
         servo_motor_data: ServoMotorData,
         time: f32,
-        pos: f32,
-        vel: f32,
-        acc: f32,
     }
     impl DummyServoMotor {
         fn new(start_state: Datum<State>) -> DummyServoMotor {
             DummyServoMotor {
                 servo_motor_data: ServoMotorData::new(start_state.clone()),
                 time: start_state.time,
-                pos: start_state.value.position,
-                vel: start_state.value.velocity,
-                acc: start_state.value.acceleration,
             }
         }
     }
@@ -252,20 +246,14 @@ fn servo_motor() {
         fn device_get_time(&mut self) -> f32 {
             self.time
         }
-        fn device_set_acceleration(&mut self, acceleration: f32) {
+        fn device_set_acceleration(&mut self, _acceleration: f32) {
             self.time += 1.0;
-            self.acc = acceleration;
         }
-        fn device_set_velocity(&mut self, velocity: f32) {
+        fn device_set_velocity(&mut self, _velocity: f32) {
             self.time += 1.0;
-            self.acc = 0.0;
-            self.vel = velocity;
         }
-        fn device_set_position(&mut self, position: f32) {
+        fn device_set_position(&mut self, _position: f32) {
             self.time += 1.0;
-            self.acc = 0.0;
-            self.vel = 0.0;
-            self.pos = position;
         }
     }
     let mut my_servo_motor = DummyServoMotor::new(Datum::new(1.0, State::new(2.0, 3.0, 4.0)));
@@ -292,4 +280,18 @@ fn servo_motor() {
     assert_eq!(output.value.position, 7.0);
     assert_eq!(output.value.velocity, 0.0);
     assert_eq!(output.value.acceleration, 0.0);
+}
+#[test]
+fn non_feedback_motor() {
+    struct DummyNonFeedbackMotor {}
+    impl DummyNonFeedbackMotor {
+        fn new() -> DummyNonFeedbackMotor {
+            DummyNonFeedbackMotor {}
+        }
+    }
+    impl NonFeedbackMotor for DummyNonFeedbackMotor {
+        fn set_power(&mut self, _power: f32) {}
+    }
+    let mut my_non_feedback_motor = DummyNonFeedbackMotor::new();
+    my_non_feedback_motor.set_power(0.39);
 }
