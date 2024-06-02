845f47b86c4cfea9d485d03b69d867123fc5c9c3
Uxugin <git@uxugin.com>
2024-06-02 12:52:49 -0600

    remove unneeded uses in tests/devices.rs

--- tests/devices.rs
+++ tests/devices.rs
@@ -1,13 +1,3 @@
-#[cfg(feature = "std")]
-use std::rc::Rc;
-#[cfg(feature = "std")]
-use std::cell::RefCell;
-#[cfg(not(feature = "std"))]
-extern crate alloc;
-#[cfg(not(feature = "std"))]
-use alloc::rc::Rc;
-#[cfg(not(feature = "std"))]
-use core::cell::RefCell;
 use rrtk::*;
 #[test]
 fn devices() {
