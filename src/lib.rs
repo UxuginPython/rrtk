289bd71829d91ebc6158785e9a357c1b01a60a2d
Uxugin <git@uxugin.com>
2024-06-10 02:59:22 -0600

    remove unneeded error

--- src/lib.rs
+++ src/lib.rs
@@ -42,8 +42,6 @@ pub mod streams;
 pub enum Error<O: Copy + Debug> {
     ///Returned when a `None` is elevated to an error by a `NoneToError`.
     FromNone,
-    ///Returned when a `TimeGetterFromStream`'s `Stream` doesn't return `Ok(Some(_))`.
-    StreamNotSome,
     ///A custom error of a user-defined type. Not created by any RRTK type but can be propagated by
     ///them.
     Other(O),
