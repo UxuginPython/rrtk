df4e9709a7862b56e717d9c4873cf9bdcb1462ab
Uxugin <git@uxugin.com>
2024-07-27 22:28:13 -0600

    IfElseStream

--- src/streams/flow.rs
+++ src/streams/flow.rs
@@ -45,3 +45,38 @@ impl<T, E: Copy + Debug> Updatable<E> for IfStream<T, E> {
         Ok(())
     }
 }
+///Returns the output of one input if a `Getter<bool, _>` returns `Ok(Some(true))` and another if
+///it returns `Ok(Some(false))`. Returns `Ok(None)` if the `Getter<bool, _>` does.
+pub struct IfElseStream<T, E: Copy + Debug> {
+    condition: InputGetter<bool, E>,
+    true_output: InputGetter<T, E>,
+    false_output: InputGetter<T, E>,
+}
+impl<T, E: Copy + Debug> IfElseStream<T, E> {
+    ///Constructor for `IfElseStream`.
+    pub fn new(condition: InputGetter<bool, E>, true_output: InputGetter<T, E>, false_output: InputGetter<T, E>) -> Self {
+        Self {
+            condition: condition,
+            true_output: true_output,
+            false_output: false_output,
+        }
+    }
+}
+impl<T, E: Copy + Debug> Getter<T, E> for IfElseStream<T, E> {
+    fn get(&self) -> Output<T, E> {
+        let condition = match self.condition.borrow().get()? {
+            Some(output) => output.value,
+            None => return Ok(None),
+        };
+        if condition {
+            self.true_output.borrow().get()
+        } else {
+            self.false_output.borrow().get()
+        }
+    }
+}
+impl<T, E: Copy + Debug> Updatable<E> for IfElseStream<T, E> {
+    fn update(&mut self) -> NothingOrError<E> {
+        Ok(())
+    }
+}
