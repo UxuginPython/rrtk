685168e617bfdae5e6f616ab06a5650cf8f03384
Uxugin <git@uxugin.com>
2024-08-19 02:15:25 -0600

    make ActuatorWrapper use Terminal TerminalData get

--- src/devices/wrappers.rs
+++ src/devices/wrappers.rs
@@ -45,32 +45,8 @@ impl<T: Settable<TerminalData, E>, E: Copy + Debug> Updatable<E>
 {
     fn update(&mut self) -> NothingOrError<E> {
         self.update_terminals()?;
-        let terminal_borrow = self.terminal.borrow();
-        let command: Option<Datum<Command>> = terminal_borrow.get_last_request();
-        let state: Option<Datum<State>> = terminal_borrow.get().expect("Terminal get always returns Ok");
-        let (mut time, command) = match command {
-            Some(datum_command) => (Some(datum_command.time), Some(datum_command.value)),
-            None => (None, None),
-        };
-        //This syntax is a bit complex. Basically, we need to shadow the state variable, but that
-        //won't work if we just have two separate statements in the branches. We also want to set
-        //the time to the state's time if it has one, otherwise keeping the possible command time.
-        //This is because the state's is likely to be newer.
-        let state = match state {
-            Some(datum_state) => {
-                time = Some(datum_state.time);
-                Some(datum_state.value)
-            }
-            None => None,
-        };
-        match time {
-            Some(time) => {
-                self.inner.set(TerminalData {
-                    time: time,
-                    command: command,
-                    state: state,
-                })?;
-            }
+        match self.terminal.borrow().get().expect("Terminal TerminalData get always returns Ok") {
+            Some(terminal_data) => self.inner.set(terminal_data.value)?,
             None => {}
         }
         self.inner.update()?;
