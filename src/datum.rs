66a71981b3ba513b4e4b5d80d06d95fd1fa82233
Uxugin <git@uxugin.com>
2024-09-04 00:08:55 -0600

    move datum and state to their own files since they have so many impl blocks

---
+++ src/datum.rs
@@ -0,0 +1,253 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+/*
+Copyright 2024 UxuginPython on GitHub
+
+     This file is part of Rust Robotics ToolKit.
+
+    Rust Robotics ToolKit is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, version 3.
+
+    Rust Robotics ToolKit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with Rust Robotics ToolKit. If not, see <https://www.gnu.org/licenses/>.
+*/
+use crate::*;
+///A container for a time and something else, usually an `f32` or a `State`.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Datum<T> {
+    ///Timestamp for the datum. This should probably be absolute.
+    pub time: i64,
+    ///The thing with the timestamp.
+    pub value: T,
+}
+impl<T> Datum<T> {
+    ///Constructor for Datum type.
+    pub const fn new(time: i64, value: T) -> Datum<T> {
+        Datum {
+            time: time,
+            value: value,
+        }
+    }
+}
+//Unfortunately implementing the ops traits is really awkward here and has unnecessary restrictions
+//because of needing to provide implementations for T and Datum<T>. If we ever get negative trait
+//bounds, it will be possible to provide a much better generic implementation for cases where the
+//type of other is not necessarily the same as T. Additionally, I may just be doing it wrong.
+//Regardless, for now, these are only implemented where other is T or Datum<T> and not a more
+//generic type. A special case for State is provided due to its Mul<f32> and Div<f32>
+//implementations.
+impl<T: Not<Output = O>, O> Not for Datum<T> {
+    type Output = Datum<O>;
+    fn not(self) -> Datum<O> {
+        Datum::new(self.time, !self.value)
+    }
+}
+impl<T: Neg<Output = O>, O> Neg for Datum<T> {
+    type Output = Datum<O>;
+    fn neg(self) -> Datum<O> {
+        Datum::new(self.time, -self.value)
+    }
+}
+impl<T: Add<Output = O>, O> Add for Datum<T> {
+    type Output = Datum<O>;
+    fn add(self, other: Self) -> Datum<O> {
+        let output_value = self.value + other.value;
+        let output_time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+        Datum::new(output_time, output_value)
+    }
+}
+impl<T: AddAssign> AddAssign for Datum<T> {
+    fn add_assign(&mut self, other: Self) {
+        self.value += other.value;
+        self.time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+    }
+}
+impl<T: Add<Output = O>, O> Add<T> for Datum<T> {
+    type Output = Datum<O>;
+    fn add(self, other: T) -> Datum<O> {
+        let output_value = self.value + other;
+        Datum::new(self.time, output_value)
+    }
+}
+impl<T: AddAssign> AddAssign<T> for Datum<T> {
+    fn add_assign(&mut self, other: T) {
+        self.value += other;
+    }
+}
+impl<T: Sub<Output = O>, O> Sub for Datum<T> {
+    type Output = Datum<O>;
+    fn sub(self, other: Self) -> Datum<O> {
+        let output_value = self.value - other.value;
+        let output_time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+        Datum::new(output_time, output_value)
+    }
+}
+impl<T: SubAssign> SubAssign for Datum<T> {
+    fn sub_assign(&mut self, other: Self) {
+        self.value -= other.value;
+        self.time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+    }
+}
+impl<T: Sub<Output = O>, O> Sub<T> for Datum<T> {
+    type Output = Datum<O>;
+    fn sub(self, other: T) -> Datum<O> {
+        let output_value = self.value - other;
+        Datum::new(self.time, output_value)
+    }
+}
+impl<T: SubAssign> SubAssign<T> for Datum<T> {
+    fn sub_assign(&mut self, other: T) {
+        self.value -= other;
+    }
+}
+impl<T: Mul<Output = O>, O> Mul for Datum<T> {
+    type Output = Datum<O>;
+    fn mul(self, other: Self) -> Datum<O> {
+        let output_value = self.value * other.value;
+        let output_time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+        Datum::new(output_time, output_value)
+    }
+}
+impl<T: MulAssign> MulAssign for Datum<T> {
+    fn mul_assign(&mut self, other: Self) {
+        self.value *= other.value;
+        self.time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+    }
+}
+impl<T: Mul<Output = O>, O> Mul<T> for Datum<T> {
+    type Output = Datum<O>;
+    fn mul(self, other: T) -> Datum<O> {
+        let output_value = self.value * other;
+        Datum::new(self.time, output_value)
+    }
+}
+impl<T: MulAssign> MulAssign<T> for Datum<T> {
+    fn mul_assign(&mut self, other: T) {
+        self.value *= other;
+    }
+}
+impl<T: Div<Output = O>, O> Div for Datum<T> {
+    type Output = Datum<O>;
+    fn div(self, other: Self) -> Datum<O> {
+        let output_value = self.value / other.value;
+        let output_time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+        Datum::new(output_time, output_value)
+    }
+}
+impl<T: DivAssign> DivAssign for Datum<T> {
+    fn div_assign(&mut self, other: Self) {
+        self.value /= other.value;
+        self.time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+    }
+}
+impl<T: Div<Output = O>, O> Div<T> for Datum<T> {
+    type Output = Datum<O>;
+    fn div(self, other: T) -> Datum<O> {
+        let output_value = self.value / other;
+        Datum::new(self.time, output_value)
+    }
+}
+impl<T: DivAssign> DivAssign<T> for Datum<T> {
+    fn div_assign(&mut self, other: T) {
+        self.value /= other;
+    }
+}
+impl Mul<Datum<f32>> for Datum<State> {
+    type Output = Self;
+    fn mul(self, other: Datum<f32>) -> Self {
+        let output_value = self.value * other.value;
+        let output_time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+        Datum::new(output_time, output_value)
+    }
+}
+impl MulAssign<Datum<f32>> for Datum<State> {
+    fn mul_assign(&mut self, other: Datum<f32>) {
+        self.value *= other.value;
+        self.time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+    }
+}
+impl Mul<f32> for Datum<State> {
+    type Output = Self;
+    fn mul(self, other: f32) -> Self {
+        let output_value = self.value * other;
+        Datum::new(self.time, output_value)
+    }
+}
+impl MulAssign<f32> for Datum<State> {
+    fn mul_assign(&mut self, other: f32) {
+        self.value *= other;
+    }
+}
+impl Div<Datum<f32>> for Datum<State> {
+    type Output = Self;
+    fn div(self, other: Datum<f32>) -> Self {
+        let output_value = self.value / other.value;
+        let output_time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+        Datum::new(output_time, output_value)
+    }
+}
+impl DivAssign<Datum<f32>> for Datum<State> {
+    fn div_assign(&mut self, other: Datum<f32>) {
+        self.value /= other.value;
+        self.time = if self.time >= other.time {
+            self.time
+        } else {
+            other.time
+        };
+    }
+}
+impl Div<f32> for Datum<State> {
+    type Output = Self;
+    fn div(self, other: f32) -> Self {
+        let output_value = self.value / other;
+        Datum::new(self.time, output_value)
+    }
+}
+impl DivAssign<f32> for Datum<State> {
+    fn div_assign(&mut self, other: f32) {
+        self.value /= other;
+    }
+}
---
+++ src/state.rs
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+/*
+Copyright 2024 UxuginPython on GitHub
+
+     This file is part of Rust Robotics ToolKit.
+
+    Rust Robotics ToolKit is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, version 3.
+
+    Rust Robotics ToolKit is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with Rust Robotics ToolKit. If not, see <https://www.gnu.org/licenses/>.
+*/
+use crate::*;
+///A one-dimensional motion state with position, velocity, and acceleration.
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+pub struct State {
+    ///Where you are.
+    pub position: f32,
+    ///How fast you're going.
+    pub velocity: f32,
+    ///How fast how fast you're going's changing.
+    pub acceleration: f32,
+}
+impl State {
+    ///Constructor for `State`.
+    pub const fn new(position: f32, velocity: f32, acceleration: f32) -> State {
+        State {
+            position: position,
+            velocity: velocity,
+            acceleration: acceleration,
+        }
+    }
+    ///Calculate the future state assuming a constant acceleration.
+    pub fn update(&mut self, delta_time: i64) {
+        let delta_time = delta_time as f32;
+        let new_velocity = self.velocity + delta_time * self.acceleration;
+        let new_position = self.position + delta_time * (self.velocity + new_velocity) / 2.0;
+        self.position = new_position;
+        self.velocity = new_velocity;
+    }
+    ///Set the acceleration.
+    pub fn set_constant_acceleration(&mut self, acceleration: f32) {
+        self.acceleration = acceleration;
+    }
+    ///Set the velocity to a given value and acceleration to zero.
+    pub fn set_constant_velocity(&mut self, velocity: f32) {
+        self.acceleration = 0.0;
+        self.velocity = velocity;
+    }
+    ///Set the position to a given value and the velocity and acceleration to zero.
+    pub fn set_constant_position(&mut self, position: f32) {
+        self.acceleration = 0.0;
+        self.velocity = 0.0;
+        self.position = position;
+    }
+    ///State contains a position, velocity, and acceleration. This gets the respective field of a
+    ///given position derivative.
+    pub fn get_value(&self, position_derivative: PositionDerivative) -> f32 {
+        match position_derivative {
+            PositionDerivative::Position => self.position,
+            PositionDerivative::Velocity => self.velocity,
+            PositionDerivative::Acceleration => self.acceleration,
+        }
+    }
+}
+impl Neg for State {
+    type Output = Self;
+    fn neg(self) -> Self {
+        State::new(-self.position, -self.velocity, -self.acceleration)
+    }
+}
+impl Add for State {
+    type Output = Self;
+    fn add(self, other: State) -> Self {
+        State::new(
+            self.position + other.position,
+            self.velocity + other.velocity,
+            self.acceleration + other.acceleration,
+        )
+    }
+}
+impl Sub for State {
+    type Output = Self;
+    fn sub(self, other: State) -> Self {
+        State::new(
+            self.position - other.position,
+            self.velocity - other.velocity,
+            self.acceleration - other.acceleration,
+        )
+    }
+}
+impl Mul<f32> for State {
+    type Output = Self;
+    fn mul(self, coef: f32) -> Self {
+        State::new(
+            self.position * coef,
+            self.velocity * coef,
+            self.acceleration * coef,
+        )
+    }
+}
+impl Div<f32> for State {
+    type Output = Self;
+    fn div(self, dvsr: f32) -> Self {
+        State::new(
+            self.position / dvsr,
+            self.velocity / dvsr,
+            self.acceleration / dvsr,
+        )
+    }
+}
+impl AddAssign for State {
+    fn add_assign(&mut self, other: State) {
+        *self = *self + other;
+    }
+}
+impl SubAssign for State {
+    fn sub_assign(&mut self, other: State) {
+        *self = *self - other;
+    }
+}
+impl MulAssign<f32> for State {
+    fn mul_assign(&mut self, coef: f32) {
+        *self = *self * coef;
+    }
+}
+impl DivAssign<f32> for State {
+    fn div_assign(&mut self, dvsr: f32) {
+        *self = *self / dvsr;
+    }
+}
+///Get the newer of two `Datum` objects.
+pub fn latest<T>(dat1: Datum<T>, dat2: Datum<T>) -> Datum<T> {
+    if dat1.time >= dat2.time {
+        dat1
+    } else {
+        dat2
+    }
+}
