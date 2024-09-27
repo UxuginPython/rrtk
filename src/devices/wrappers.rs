5840c8320c6f1686e6861abefca9821cc531c55e
Uxugin <git@uxugin.com>
2024-09-26 21:51:00 -0600

    comment out problematic PIDWrapper

--- src/devices/wrappers.rs
+++ src/devices/wrappers.rs
@@ -90,7 +90,7 @@ impl<T: Getter<State, E>, E: Copy + Debug> Updatable<E> for GetterStateDeviceWra
         Ok(())
     }
 }
-///Connect a `Settable<f32, E>` motor to the device system through a `CommandPID`. See
+/*///Connect a `Settable<f32, E>` motor to the device system through a `CommandPID`. See
 ///`streams::control::CommandPID` documentation for more information about how this works.
 pub struct PIDWrapper<'a, T: Settable<f32, E>, E: Copy + Debug + 'static> {
     terminal: RefCell<Terminal<'a, E>>,
@@ -171,4 +171,4 @@ impl<T: Settable<f32, E>, E: Copy + Debug + 'static> Updatable<E> for PIDWrapper
         self.inner.update()?;
         Ok(())
     }
-}
+}*/
