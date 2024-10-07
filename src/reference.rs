f484208a04adb97f072196140501f1dd4d3b8a5f
Uxugin <git@uxugin.com>
2024-10-07 04:27:58 -0600

    make Reference and the like direct public enums (remove the wrapper struct)

--- src/reference.rs
+++ src/reference.rs
@@ -5,14 +5,23 @@ use crate::*;
 use core::cell::{Ref, RefMut};
 #[cfg(feature = "std")]
 use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
-enum InternalBorrow<'a, T: ?Sized> {
+///An immutable borrow of an RRTK `Reference`, similar to `Ref` for a `RefCell`.
+///This is marked as non-exhaustive because some variants are only available with some features.
+///This means that if you write a `match` without all the features enabled, it won't cover all the
+///variants if another crate in the tree enables more features. This is a problem because features
+///are additive, so it is marked as non-exhaustive to remedy this.
+#[non_exhaustive]
+pub enum Borrow<'a, T: ?Sized> {
+    ///A raw immutable pointer.
     Ptr(*const T, PhantomData<&'a ()>),
+    ///An immutable borrow of an `Rc<RefCell<T>>`.
     #[cfg(feature = "alloc")]
     RefCellRef(Ref<'a, T>),
+    ///An `RwLockReadGuard`.
     #[cfg(feature = "std")]
     RwLockReadGuard(RwLockReadGuard<'a, T>),
 }
-impl<T: ?Sized> Deref for InternalBorrow<'_, T> {
+impl<T: ?Sized> Deref for Borrow<'_, T> {
     type Target = T;
     fn deref(&self) -> &T {
         match self {
@@ -24,22 +33,23 @@ impl<T: ?Sized> Deref for InternalBorrow<'_, T> {
         }
     }
 }
-///An immutable borrow of an RRTK `Reference`, similar to `Ref` for a `RefCell`.
-pub struct Borrow<'a, T: ?Sized>(InternalBorrow<'a, T>);
-impl<T: ?Sized> Deref for Borrow<'_, T> {
-    type Target = T;
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-enum InternalBorrowMut<'a, T: ?Sized> {
+///A mutable borrow of an RRTK `Reference`, similar to `RefMut` for a `RefCell`.
+///This is marked as non-exhaustive because some variants are only available with some features.
+///This means that if you write a `match` without all the features enabled, it won't cover all the
+///variants if another crate in the tree enables more features. This is a problem because features
+///are additive, so it is marked as non-exhaustive to remedy this.
+#[non_exhaustive]
+pub enum BorrowMut<'a, T: ?Sized> {
+    ///A raw mutable pointer.
     Ptr(*mut T, PhantomData<&'a ()>),
+    ///A mutable borrow of an `Rc<RefCell<T>>`.
     #[cfg(feature = "alloc")]
     RefCellRefMut(RefMut<'a, T>),
+    ///An `RwLockWriteGuard`.
     #[cfg(feature = "std")]
     RwLockWriteGuard(RwLockWriteGuard<'a, T>),
 }
-impl<T: ?Sized> Deref for InternalBorrowMut<'_, T> {
+impl<T: ?Sized> Deref for BorrowMut<'_, T> {
     type Target = T;
     fn deref(&self) -> &T {
         match self {
@@ -51,7 +61,7 @@ impl<T: ?Sized> Deref for InternalBorrowMut<'_, T> {
         }
     }
 }
-impl<T: ?Sized> DerefMut for InternalBorrowMut<'_, T> {
+impl<T: ?Sized> DerefMut for BorrowMut<'_, T> {
     fn deref_mut(&mut self) -> &mut T {
         match self {
             Self::Ptr(ptr, _) => unsafe { &mut **ptr },
@@ -62,35 +72,49 @@ impl<T: ?Sized> DerefMut for InternalBorrowMut<'_, T> {
         }
     }
 }
-///A mutable borrow of an RRTK `Reference`, similar to `RefMut` for a `RefCell`.
-pub struct BorrowMut<'a, T: ?Sized>(InternalBorrowMut<'a, T>);
-impl<T: ?Sized> Deref for BorrowMut<'_, T> {
-    type Target = T;
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-impl<T: ?Sized> DerefMut for BorrowMut<'_, T> {
-    fn deref_mut(&mut self) -> &mut T {
-        &mut self.0
-    }
-}
-enum InternalReference<T: ?Sized> {
+///A special enum with variants for different kinds of references depending on your platform and
+///code structure. (Some variants are alloc- or std-only.)
+///This is marked as non-exhaustive because some variants are only available with some features.
+///This means that if you write a `match` without all the features enabled, it won't cover all the
+///variants if another crate in the tree enables more features. This is a problem because features
+///are additive, so it is marked as non-exhaustive to remedy this.
+#[non_exhaustive]
+pub enum Reference<T: ?Sized> {
+    ///A raw mutable pointer.
     Ptr(*mut T),
+    ///An `Rc<RefCell<T>>`.
     #[cfg(feature = "alloc")]
     RcRefCell(Rc<RefCell<T>>),
+    ///A raw immutable pointer to an `RwLock<T>`.
     #[cfg(feature = "std")]
     PtrRwLock(*const RwLock<T>),
 }
-impl<T: ?Sized> InternalReference<T> {
-    fn borrow(&self) -> InternalBorrow<'_, T> {
+impl<T: ?Sized> Reference<T> {
+    ///Create a `Reference` from a raw mutable pointer. Good if you're not multithreading and you
+    ///want to avoid dynamic allocation. Making the object static is strongly recommended if you
+    ///use this.
+    pub const unsafe fn from_ptr(ptr: *mut T) -> Self {
+        Self::Ptr(ptr)
+    }
+    ///Create a `Reference` from an `Rc<RefCell<T>>`.
+    #[cfg(feature = "alloc")]
+    pub const fn from_rc_refcell(rc_refcell: Rc<RefCell<T>>) -> Self {
+        Self::RcRefCell(rc_refcell)
+    }
+    ///Create a `Reference` from a `*const RwLock<T>`.
+    #[cfg(feature = "std")]
+    pub const unsafe fn from_rwlock_ptr(ptr_rwlock: *const RwLock<T>) -> Self {
+        Self::PtrRwLock(ptr_rwlock)
+    }
+    ///Immutably borrow the reference like a `RefCell`.
+    pub fn borrow(&self) -> Borrow<'_, T> {
         match self {
-            Self::Ptr(ptr) => InternalBorrow::Ptr(*ptr, PhantomData),
+            Self::Ptr(ptr) => Borrow::Ptr(*ptr, PhantomData),
             #[cfg(feature = "alloc")]
-            Self::RcRefCell(rc_refcell) => InternalBorrow::RefCellRef(rc_refcell.borrow()),
+            Self::RcRefCell(rc_refcell) => Borrow::RefCellRef(rc_refcell.borrow()),
             #[cfg(feature = "std")]
             Self::PtrRwLock(ptr_rw_lock) => unsafe {
-                InternalBorrow::RwLockReadGuard(
+                Borrow::RwLockReadGuard(
                     (**ptr_rw_lock)
                         .read()
                         .expect("RRTK Reference borrow failed to get RwLock read lock"),
@@ -98,16 +122,15 @@ impl<T: ?Sized> InternalReference<T> {
             },
         }
     }
-    fn borrow_mut(&self) -> InternalBorrowMut<'_, T> {
+    ///Mutably borrow the reference like a `RefCell`.
+    pub fn borrow_mut(&self) -> BorrowMut<'_, T> {
         match self {
-            Self::Ptr(ptr) => InternalBorrowMut::Ptr(*ptr, PhantomData),
+            Self::Ptr(ptr) => BorrowMut::Ptr(*ptr, PhantomData),
             #[cfg(feature = "alloc")]
-            Self::RcRefCell(rc_refcell) => {
-                InternalBorrowMut::RefCellRefMut(rc_refcell.borrow_mut())
-            }
+            Self::RcRefCell(rc_refcell) => BorrowMut::RefCellRefMut(rc_refcell.borrow_mut()),
             #[cfg(feature = "std")]
             Self::PtrRwLock(ptr_rw_lock) => unsafe {
-                InternalBorrowMut::RwLockWriteGuard(
+                BorrowMut::RwLockWriteGuard(
                     (**ptr_rw_lock)
                         .write()
                         .expect("RRTK Reference mutable borrow failed to get RwLock write lock"),
@@ -116,82 +139,42 @@ impl<T: ?Sized> InternalReference<T> {
         }
     }
 }
-impl<T: ?Sized> Clone for InternalReference<T> {
+impl<T: ?Sized> Clone for Reference<T> {
     fn clone(&self) -> Self {
         match self {
             Self::Ptr(ptr) => Self::Ptr(*ptr),
+            #[cfg(feature = "alloc")]
             Self::RcRefCell(rc_refcell) => Self::RcRefCell(Rc::clone(&rc_refcell)),
+            #[cfg(feature = "std")]
             Self::PtrRwLock(ptr_rwlock) => Self::PtrRwLock(*ptr_rwlock),
         }
     }
 }
-///A special enum with variants for different kinds of references depending on your platform and
-///code structure. (Some variants are alloc- or std-only.)
-pub struct Reference<T: ?Sized>(InternalReference<T>);
-impl<T: ?Sized> Reference<T> {
-    ///Create a `Reference` from a raw mutable pointer. Good if you're not multithreading and you
-    ///want to avoid dynamic allocation. Making the object static is strongly recommended if you
-    ///use this.
-    pub const unsafe fn from_ptr(ptr: *mut T) -> Self {
-        Self(InternalReference::Ptr(ptr))
-    }
-    ///Create a `Reference` from an `Rc<RefCell<T>>`.
-    #[cfg(feature = "alloc")]
-    pub const fn from_rc_refcell(rc_refcell: Rc<RefCell<T>>) -> Self {
-        Self(InternalReference::RcRefCell(rc_refcell))
-    }
-    ///Create a `Reference` from a `*const RwLock<T>`.
-    #[cfg(feature = "std")]
-    pub const unsafe fn from_rwlock_ptr(ptr_rwlock: *const RwLock<T>) -> Self {
-        Self(InternalReference::PtrRwLock(ptr_rwlock))
-    }
-    ///Immutably borrow a `Reference`, similarly to how you would with a `RefCell`.
-    pub fn borrow(&self) -> Borrow<'_, T> {
-        Borrow(self.0.borrow())
-    }
-    ///Mutably borrow a `Reference`, similarly to how you would with a `RefCell`.
-    pub fn borrow_mut(&self) -> BorrowMut<'_, T> {
-        BorrowMut(self.0.borrow_mut())
-    }
-}
-impl<T: ?Sized> Clone for Reference<T> {
-    fn clone(&self) -> Self {
-        Self(self.0.clone())
-    }
-}
-#[cfg(all(not(feature = "alloc"), not(feature = "std")))]
+///If you have a `Reference<Foo>` where `Foo` implements the `Bar` trait, you may end up wanting a
+///`Reference<dyn Bar>`. To convert it, you would do this:
+///```
+///# use rrtk::*;
+///# use std::cell::RefCell;
+///# use std::rc::Rc;
+///# struct Foo;
+///# trait Bar {}
+///# impl Bar for Foo {}
+///let ref_foo = Reference::from_rc_refcell(Rc::new(RefCell::new(Foo)));
+///let ref_dyn_bar = to_dyn!(Bar, ref_foo);
+///```
 #[macro_export]
 macro_rules! to_dyn {
     ($trait:path, $was:ident) => {
-        match $was.0 {
-            InternalReference::Ptr(ptr) => Reference::from_ptr(ptr as *mut dyn $trait),
-        }
-    };
-}
-#[cfg(all(feature = "alloc", not(feature = "std")))]
-#[macro_export]
-macro_rules! to_dyn {
-    ($trait:path, $was:ident) => {
-        match $was.0 {
-            InternalReference::Ptr(ptr) => Reference::from_ptr(ptr as *mut dyn $trait),
-            InternalReference::RcRefCell(rc_refcell) => {
+        match $was {
+            Reference::Ptr(ptr) => unsafe { Reference::from_ptr(ptr as *mut dyn $trait) },
+            #[cfg(feature = "alloc")]
+            Reference::RcRefCell(rc_refcell) => {
                 Reference::from_rc_refcell(rc_refcell as Rc<RefCell<dyn $trait>>)
             }
-        }
-    };
-}
-#[cfg(feature = "std")]
-#[macro_export]
-macro_rules! to_dyn {
-    ($trait:path, $was:ident) => {
-        match $was.0 {
-            InternalReference::Ptr(ptr) => Reference::from_ptr(ptr as *mut dyn $trait),
-            InternalReference::RcRefCell(rc_refcell) => {
-                Reference::from_rc_refcell(rc_refcell as Rc<RefCell<dyn $trait>>)
-            }
-            InternalReference::PtrRwLock(ptr_rw_lock) => {
+            #[cfg(feature = "std")]
+            Reference::PtrRwLock(ptr_rw_lock) => unsafe {
                 Reference::from_rwlock_ptr(ptr_rw_lock as *const std::sync::RwLock<dyn $trait>)
-            }
+            },
         }
     };
 }
