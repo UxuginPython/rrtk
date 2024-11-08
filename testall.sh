b0d7fcba587724bdad58d2326009fdeb356be14c
Uxugin <git@uxugin.com>
2024-11-08 14:33:54 -0700

    regenerate testall.sh and checkall.sh

--- testall.sh
+++ testall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated automatically by rrtk 0.5.0
+#Generated automatically by rrtk 0.6.0-alpha.0
 set -e
 echo
 cargo test --no-default-features
@@ -9,7 +9,31 @@ echo std
 cargo test --no-default-features --features std
 echo devices
 cargo test --no-default-features --features devices
+echo dim_check_debug
+cargo test --no-default-features --features dim_check_debug
+echo dim_check_release
+cargo test --no-default-features --features dim_check_release
 echo alloc devices
 cargo test --no-default-features --features alloc,devices
+echo alloc dim_check_debug
+cargo test --no-default-features --features alloc,dim_check_debug
+echo alloc dim_check_release
+cargo test --no-default-features --features alloc,dim_check_release
 echo std devices
 cargo test --no-default-features --features std,devices
+echo std dim_check_debug
+cargo test --no-default-features --features std,dim_check_debug
+echo std dim_check_release
+cargo test --no-default-features --features std,dim_check_release
+echo devices dim_check_debug
+cargo test --no-default-features --features devices,dim_check_debug
+echo devices dim_check_release
+cargo test --no-default-features --features devices,dim_check_release
+echo alloc devices dim_check_debug
+cargo test --no-default-features --features alloc,devices,dim_check_debug
+echo alloc devices dim_check_release
+cargo test --no-default-features --features alloc,devices,dim_check_release
+echo std devices dim_check_debug
+cargo test --no-default-features --features std,devices,dim_check_debug
+echo std devices dim_check_release
+cargo test --no-default-features --features std,devices,dim_check_release
