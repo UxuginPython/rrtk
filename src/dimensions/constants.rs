a4c767747ecb344570377220a613efe13bf11f2c
Uxugin <git@uxugin.com>
2024-11-08 13:25:37 -0700

    start documenting constants

--- src/dimensions/constants.rs
+++ src/dimensions/constants.rs
@@ -2,12 +2,19 @@
 //!positive exponent and millimeter a negative. Everything in this module is reexported both at the
 //!`dimensions` module and at the crate level.
 use crate::*;
+///The `Unit` for a quantity in inverse millimeters cubed seconds cubed (mm^-3 s^-3).
 pub const INVERSE_MILLIMETER_CUBED_SECOND_CUBED: Unit = Unit::new(-3, -3);
+///The `Unit` for a quantity in inverse millimeters cubed seconds squared (mm^-3 s^-2).
 pub const INVERSE_MILLIMETER_CUBED_SECOND_SQUARED: Unit = Unit::new(-3, -2);
+///The `Unit` for a quantity in inverse millimeters cubed seconds (mm^-3 s^-1).
 pub const INVERSE_MILLIMETER_CUBED_SECOND: Unit = Unit::new(-3, -1);
+///The `Unit` for a quantity in inverse millimeters cubed (mm^-3).
 pub const INVERSE_MILLIMETER_CUBED: Unit = Unit::new(-3, 0);
+///The `Unit` for a quantity in seconds per millimeter cubed (s/mm^3).
 pub const SECOND_PER_MILLIMETER_CUBED: Unit = Unit::new(-3, 1);
+///The `Unit` for a quantity in seconds squared per millimeter cubed (s^2/mm^3).
 pub const SECOND_SQUARED_PER_MILLIMETER_CUBED: Unit = Unit::new(-3, 2);
+///The `Unit` for a quantity in seconds cubed per millimeter cubed (s^3/mm^3).
 pub const SECOND_CUBED_PER_MILLIMETER_CUBED: Unit = Unit::new(-3, 3);
 
 pub const INVERSE_MILLIMETER_SQUARED_SECOND_CUBED: Unit = Unit::new(-2, -3);
