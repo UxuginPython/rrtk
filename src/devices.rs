0bc23f3fd0d064db56d9c3a84ea2d2c809c4af7f
Uxugin <git@uxugin.com>
2024-12-29 19:42:48 -0700

    Terminal is Getter<Datum<Command>, _> so Updatable for Axle can be simplified

--- src/devices.rs
+++ src/devices.rs
@@ -254,16 +254,7 @@ impl<const N: usize, E: Copy + Debug> Updatable<E> for Axle<'_, N, E> {
         }
         let mut maybe_datum: Option<Datum<Command>> = None;
         for i in &self.inputs {
-            match <Terminal<'_, E> as Getter<TerminalData, E>>::get(&i.borrow())? {
-                Some(gotten_datum) => match Datum::<Command>::try_from(gotten_datum.value) {
-                    Ok(gotten_datum) => {
-                        //It needs to be in a block to throw out the return value.
-                        maybe_datum.replace_if_none_or_older_than(gotten_datum);
-                    }
-                    Err(_) => (),
-                },
-                None => (),
-            }
+            maybe_datum.replace_if_none_or_older_than_option(i.borrow().get()?);
         }
         if let Some(datum) = maybe_datum {
             for i in &self.inputs {
