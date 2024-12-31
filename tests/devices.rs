e0b37638b6f7d5be3b9caed07a8bb1d32c564860
Uxugin <git@uxugin.com>
2024-12-31 06:30:54 -0700

    GearTrain command propagation testing

--- tests/devices.rs
+++ tests/devices.rs
@@ -180,14 +180,23 @@ fn gear_train_2() {
     connect(gear_train.get_terminal_1(), &terminal1);
     connect(gear_train.get_terminal_2(), &terminal2);
     assert_eq!(terminal2.borrow_mut().get(), Ok(None::<Datum<State>>));
+    assert_eq!(terminal2.borrow_mut().get(), Ok(None::<Datum<Command>>));
     terminal1
         .borrow_mut()
         .set(Datum::new(Time(0), State::new_raw(3.0, 6.0, 9.0)))
+        .unwrap();
+    terminal1
+        .borrow_mut()
+        .set(Datum::new(Time(0), Command::Position(3.0)))
         .unwrap();
     gear_train.update().unwrap();
     assert_eq!(
         terminal2.borrow_mut().get(),
         Ok(Some(Datum::new(Time(0), State::new_raw(-1.0, -2.0, -3.0))))
+    );
+    assert_eq!(
+        terminal2.borrow_mut().get(),
+        Ok(Some(Datum::new(Time(0), Command::Position(-1.0))))
     );
 }
 #[test]
@@ -198,14 +207,23 @@ fn gear_train_odd() {
     connect(gear_train.get_terminal_1(), &terminal1);
     connect(gear_train.get_terminal_2(), &terminal2);
     assert_eq!(terminal2.borrow_mut().get(), Ok(None::<Datum<State>>));
+    assert_eq!(terminal2.borrow_mut().get(), Ok(None::<Datum<Command>>));
     terminal1
         .borrow_mut()
         .set(Datum::new(Time(0), State::new_raw(2.0, 4.0, 6.0)))
+        .unwrap();
+    terminal1
+        .borrow_mut()
+        .set(Datum::new(Time(0), Command::Position(2.0)))
         .unwrap();
     gear_train.update().unwrap();
     assert_eq!(
         terminal2.borrow_mut().get(),
         Ok(Some(Datum::new(Time(0), State::new_raw(3.0, 6.0, 9.0))))
+    );
+    assert_eq!(
+        terminal2.borrow_mut().get(),
+        Ok(Some(Datum::new(Time(0), Command::Position(3.0))))
     );
 }
 #[test]
@@ -216,14 +234,23 @@ fn gear_train_even() {
     connect(gear_train.get_terminal_1(), &terminal1);
     connect(gear_train.get_terminal_2(), &terminal2);
     assert_eq!(terminal2.borrow_mut().get(), Ok(None::<Datum<State>>));
+    assert_eq!(terminal2.borrow_mut().get(), Ok(None::<Datum<Command>>));
     terminal1
         .borrow_mut()
         .set(Datum::new(Time(0), State::new_raw(2.0, 4.0, 6.0)))
+        .unwrap();
+    terminal1
+        .borrow_mut()
+        .set(Datum::new(Time(0), Command::Position(2.0)))
         .unwrap();
     gear_train.update().unwrap();
     assert_eq!(
         terminal2.borrow_mut().get(),
         Ok(Some(Datum::new(Time(0), State::new_raw(-3.0, -6.0, -9.0))))
+    );
+    assert_eq!(
+        terminal2.borrow_mut().get(),
+        Ok(Some(Datum::new(Time(0), Command::Position(-3.0))))
     );
 }
 #[test]
@@ -235,9 +262,19 @@ fn gear_train_multiple_inputs() {
         .set(Datum::new(Time(3), State::new_raw(2.0, 4.0, 6.0)))
         .unwrap();
     gear_train
+        .get_terminal_1()
+        .borrow_mut()
+        .set(Datum::new(Time(3), Command::Position(2.0)))
+        .unwrap();
+    gear_train
         .get_terminal_2()
         .borrow_mut()
         .set(Datum::new(Time(2), State::new_raw(-2.0, -4.0, -6.0)))
+        .unwrap();
+    gear_train
+        .get_terminal_2()
+        .borrow_mut()
+        .set(Datum::new(Time(2), Command::Position(-2.0)))
         .unwrap();
     gear_train.update().unwrap();
     assert_eq!(
@@ -245,8 +282,16 @@ fn gear_train_multiple_inputs() {
         Ok(Some(Datum::new(Time(3), State::new_raw(2.4, 4.8, 7.2))))
     );
     assert_eq!(
+        gear_train.get_terminal_1().borrow().get(),
+        Ok(Some(Datum::new(Time(3), Command::Position(2.0))))
+    );
+    assert_eq!(
         gear_train.get_terminal_2().borrow().get(),
         Ok(Some(Datum::new(Time(3), State::new_raw(-1.2, -2.4, -3.6))))
+    );
+    assert_eq!(
+        gear_train.get_terminal_2().borrow().get(),
+        Ok(Some(Datum::new(Time(3), Command::Position(-1.0))))
     );
 }
 #[test]
