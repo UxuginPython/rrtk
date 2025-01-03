762a804a6cb1df5e461fcc029b5a91dc82c543de
Uxugin <git@uxugin.com>
2025-01-02 19:19:29 -0700

    cool macro stuff in ValueWithoutUnit

--- src/dimensions/value_without_unit.rs
+++ src/dimensions/value_without_unit.rs
@@ -6,22 +6,30 @@ pub enum ValueWithoutUnit {
     #[cfg(feature = "error_propagation")]
     WithError(ValueWithoutUnitWithError),
 }
-impl Add for ValueWithoutUnit {
-    type Output = Self;
-    fn add(self, rhs: Self) -> Self {
-        match self {
-            Self::WithoutError(self_wo_error) => match rhs {
-                Self::WithoutError(rhs_wo_error) => {
-                    Self::WithoutError(self_wo_error + rhs_wo_error)
+macro_rules! impl_op {
+    ($op_trait: ident, $op_func: ident, $op_symbol: tt) => {
+        impl $op_trait for ValueWithoutUnit {
+            type Output = Self;
+            fn $op_func(self, rhs: Self) -> Self {
+                match self {
+                    Self::WithoutError(self_wo_error) => match rhs {
+                        Self::WithoutError(rhs_wo_error) => {
+                            Self::WithoutError(self_wo_error $op_symbol rhs_wo_error)
+                        }
+                        #[cfg(feature = "error_propagation")]
+                        Self::WithError(rhs_w_error) => Self::WithError(self_wo_error $op_symbol rhs_w_error),
+                    },
+                    #[cfg(feature = "error_propagation")]
+                    Self::WithError(self_w_error) => Self::WithError(match rhs {
+                        Self::WithoutError(rhs_wo_error) => self_w_error $op_symbol rhs_wo_error,
+                        Self::WithError(rhs_w_error) => self_w_error $op_symbol rhs_w_error,
+                    }),
                 }
-                #[cfg(feature = "error_propagation")]
-                Self::WithError(rhs_w_error) => Self::WithError(self_wo_error + rhs_w_error),
-            },
-            #[cfg(feature = "error_propagation")]
-            Self::WithError(self_w_error) => Self::WithError(match rhs {
-                Self::WithoutError(rhs_wo_error) => self_w_error + rhs_wo_error,
-                Self::WithError(rhs_w_error) => self_w_error + rhs_w_error,
-            }),
+            }
         }
     }
 }
+impl_op!(Add, add, +);
+impl_op!(Sub, sub, -);
+impl_op!(Mul, mul, *);
+impl_op!(Div, div, /);
