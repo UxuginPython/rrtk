6d35bbea01b023c5294102aaa0cbbdcc1bae84a3
Uxugin <git@uxugin.com>
2025-01-03 01:51:40 -0700

    sqrt in enhanced_float

--- src/enhanced_float.rs
+++ src/enhanced_float.rs
@@ -1,4 +1,7 @@
 #![cfg(feature = "internal_enhanced_float")]
+#[cfg(all(feature = "micromath", not(feature = "std"), not(feature = "libm")))]
+pub use micromath::F32Ext;
+
 //micromath's F32Ext is drop-in compatible with std floating point operations. However, we prefer
 //libm over micromath and std over libm, so this function definition is enabled if either std is
 //available or both micromath is available and libm is not.
@@ -9,5 +12,13 @@ pub fn powf(x: f32, y: f32) -> f32 {
 }
 #[cfg(all(feature = "libm", not(feature = "std")))]
 pub use libm::powf;
-#[cfg(all(feature = "micromath", not(feature = "std"), not(feature = "libm")))]
-pub use micromath::F32Ext;
+
+#[cfg(any(feature = "std", all(feature = "micromath", not(feature = "libm"))))]
+#[inline]
+pub fn sqrt(x: f32) -> f32 {
+    x.sqrt()
+}
+#[cfg(all(feature = "libm", not(feature = "std")))]
+pub fn sqrt(x: f32) -> f32 {
+    libm::sqrtf(x)
+}
