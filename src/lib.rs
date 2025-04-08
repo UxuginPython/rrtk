1e58ec2dcef15ea384c231027c475f2efec5cd3e
Uxugin <git@uxugin.com>
2025-04-08 14:15:32 -0600

    document CannotConvert

--- src/lib.rs
+++ src/lib.rs
@@ -71,6 +71,7 @@ pub use reference::rc_ref_cell_reference;
 #[cfg(feature = "std")]
 pub use reference::{arc_mutex_reference, arc_rw_lock_reference};
 pub use state::*;
+///The error type used when a `TryFrom` fails.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct CannotConvert;
 ///A derivative of position: position, velocity, or acceleration.
