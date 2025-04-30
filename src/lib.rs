5508fc208adff7e727f5d5b806bcfa3a39ddf232
Uxugin <git@uxugin.com>
2025-04-29 21:59:39 -0600

    experiment with implementing UGS for Rc<RefCell>

--- src/lib.rs
+++ src/lib.rs
@@ -753,3 +753,21 @@ impl<T, S: ?Sized + Settable<T, E>, E: Copy + Debug> Settable<T, E> for PointerD
         unsafe { (*self.pointer).set(value) }
     }
 }
+#[cfg(feature = "alloc")]
+impl<U: Updatable<E>, E: Copy + Debug> Updatable<E> for Rc<RefCell<U>> {
+    fn update(&mut self) -> NothingOrError<E> {
+        self.borrow_mut().update()
+    }
+}
+#[cfg(feature = "alloc")]
+impl<G: Getter<T, E>, T, E: Copy + Debug> Getter<T, E> for Rc<RefCell<G>> {
+    fn get(&self) -> Output<T, E> {
+        self.borrow().get()
+    }
+}
+#[cfg(feature = "alloc")]
+impl<S: Settable<T, E>, T, E: Copy + Debug> Settable<T, E> for Rc<RefCell<S>> {
+    fn set(&mut self, value: T) -> NothingOrError<E> {
+        self.borrow_mut().set(value)
+    }
+}
