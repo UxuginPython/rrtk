6115a0b7dba35ff03bfe8fc5c9b4e34f2fd85b8d
Uxugin <git@uxugin.com>
2025-05-30 06:50:49 -0600

    make or better too

--- src/streams/logic.rs
+++ src/streams/logic.rs
@@ -15,6 +15,12 @@ impl LogicState {
             *self = Self::NeitherReturnable;
         }
     }
+    #[inline]
+    fn not_returnable_false(&mut self) {
+        if let Self::ReturnableFalse = self {
+            *self = Self::NeitherReturnable;
+        }
+    }
 }
 pub struct GoodAndStream<const N: usize, G: Getter<bool, E>, E: Clone + Debug> {
     inputs: [G; N],
@@ -86,146 +92,26 @@ impl<const N: usize, G: Getter<bool, E>, E: Clone + Debug> Getter<bool, E>
     for GoodOrStream<N, G, E>
 {
     fn get(&self) -> Output<bool, E> {
-        let mut have_returnable_false = true;
-        let mut have_returnable_true = false;
+        let mut logic_state = LogicState::ReturnableFalse;
         let mut time = Time::ZERO;
         for getter in &self.inputs {
             match getter.get()? {
-                None => have_returnable_false = false,
+                None => logic_state.not_returnable_false(),
                 Some(datum) => {
                     if datum.time > time {
                         time = datum.time;
                     }
                     if datum.value {
-                        have_returnable_false = false;
-                        have_returnable_true = true;
+                        logic_state = LogicState::ReturnableTrue;
                     }
                 }
             }
         }
-        debug_assert!(!(have_returnable_true && have_returnable_false));
-        if have_returnable_true {
-            return Ok(Some(Datum::new(time, true)));
+        match logic_state {
+            LogicState::ReturnableTrue => Ok(Some(Datum::new(time, true))),
+            LogicState::ReturnableFalse => Ok(Some(Datum::new(time, false))),
+            LogicState::NeitherReturnable => Ok(None),
         }
-        if have_returnable_false {
-            return Ok(Some(Datum::new(time, false)));
-        }
-        Ok(None)
-    }
-}
-enum OrState {
-    DefinitelyTrue, //An input returned true.
-    MaybeFalse,     //An input returned None and no input has returned true, so we can't assume an
-    //output.
-    ReturnableFalse, //No input has returned None or true.
-}
-impl OrState {
-    #[inline]
-    fn none(&mut self) {
-        if let OrState::ReturnableFalse = self {
-            *self = OrState::MaybeFalse
-        }
-    }
-}
-///Performs an or operation on two boolean getters. This will return [`None`] if it can't verify that
-///the result should be [`true`] or [`false`].
-///| Input 1         | Input 2       | [`OrStream`]      |
-///|-----------------|---------------|-------------------|
-///| [`Some(false)`] | [`Some(false)`] | [`Some(false)`] |
-///| [`None`]        | [`Some(false)`] | [`None`]        |
-///| [`Some(true)`]  | [`Some(false)`] | [`Some(true)`]  |
-///| [`Some(false)`] | [`None`]        | [`None`]        |
-///| [`None`]        | [`None`]        | [`None`]        |
-///| [`Some(true)`]  | [`None`]        | [`Some(true)`]  |
-///| [`Some(false)`] | [`Some(true)`]  | [`Some(true)`]  |
-///| [`None`]        | [`Some(true)`]  | [`Some(true)`]  |
-///| [`Some(true)`]  | [`Some(true)`]  | [`Some(true)`]  |
-pub struct OrStream<G1, G2, E>
-where
-    G1: Getter<bool, E>,
-    G2: Getter<bool, E>,
-    E: Clone + Debug,
-{
-    input1: G1,
-    input2: G2,
-    phantom_e: PhantomData<E>,
-}
-impl<G1, G2, E> OrStream<G1, G2, E>
-where
-    G1: Getter<bool, E>,
-    G2: Getter<bool, E>,
-    E: Clone + Debug,
-{
-    ///Constructor for [`OrStream`].
-    pub const fn new(input1: G1, input2: G2) -> Self {
-        Self {
-            input1,
-            input2,
-            phantom_e: PhantomData,
-        }
-    }
-}
-impl<G1, G2, E> Getter<bool, E> for OrStream<G1, G2, E>
-where
-    G1: Getter<bool, E>,
-    G2: Getter<bool, E>,
-    E: Clone + Debug,
-{
-    fn get(&self) -> Output<bool, E> {
-        let gotten1 = self.input1.get()?;
-        let gotten2 = self.input2.get()?;
-        let mut time = None;
-        let mut or_state = OrState::ReturnableFalse;
-        match gotten1 {
-            Some(datum) => {
-                time = Some(datum.time);
-                if datum.value {
-                    or_state = OrState::DefinitelyTrue;
-                }
-            }
-            None => {
-                or_state.none();
-            }
-        }
-        match gotten2 {
-            Some(datum) => {
-                match time {
-                    Some(existing) => {
-                        if datum.time > existing {
-                            time = Some(datum.time);
-                        }
-                    }
-                    None => time = Some(datum.time),
-                }
-                if datum.value {
-                    or_state = OrState::DefinitelyTrue;
-                }
-            }
-            None => {
-                or_state.none();
-            }
-        }
-        let time = match time {
-            Some(time) => time,
-            None => return Ok(None),
-        };
-        match or_state {
-            OrState::DefinitelyTrue => Ok(Some(Datum::new(time, true))),
-            OrState::MaybeFalse => Ok(None),
-            OrState::ReturnableFalse => Ok(Some(Datum::new(time, false))),
-        }
-    }
-}
-impl<G1, G2, E> Updatable<E> for OrStream<G1, G2, E>
-where
-    G1: Getter<bool, E>,
-    G2: Getter<bool, E>,
-    E: Clone + Debug,
-{
-    fn update(&mut self) -> NothingOrError<E> {
-        self.input1.update()?;
-        self.input2.update()?;
-        Ok(())
     }
 }
 ///Performs a not operation on a boolean getter.
