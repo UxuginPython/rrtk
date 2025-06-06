96e5ceb80b15e471316ff59b3da7df8521fbe1f0
Uxugin <git@uxugin.com>
2025-06-05 22:00:56 -0600

    new set_constant_(position|velocity|acceleration)

--- src/state.rs
+++ src/state.rs
@@ -46,70 +46,38 @@ impl State {
         self.position = new_position;
         self.velocity = new_velocity;
     }
-    /*///Set the acceleration with a [`Quantity`]. With dimension checking enabled, sets the
-    ///acceleration and returns [`Ok`] if the argument's [`Unit`] is correct, otherwise leaves it
-    ///unchanged and returns [`Err`]. With dimension checking disabled, always sets the acceleration
-    ///to the [`Quantity`]'s value and returns [`Ok`], ignoring the [`Unit`].
+    ///Set the acceleration.
+    #[inline]
     pub const fn set_constant_acceleration(
         &mut self,
-        acceleration: Quantity,
-    ) -> Result<(), error::UnitInvalid> {
-        if acceleration
-            .unit
-            .eq_assume_true(&MILLIMETER_PER_SECOND_SQUARED)
-        {
-            self.acceleration = acceleration.value;
-            Ok(())
-        } else {
-            Err(error::UnitInvalid)
-        }
-    }*/
+        acceleration: MillimeterPerSecondSquared<f32>,
+    ) {
+        self.acceleration = acceleration;
+    }
     ///Set the acceleration with an [`f32`] of millimeters per second squared.
     #[inline]
     pub const fn set_constant_acceleration_raw(&mut self, acceleration: f32) {
         self.acceleration = MillimeterPerSecondSquared::new(acceleration);
     }
-    /*///Set the velocity to a given value with a [`Quantity`], and set acceleration to zero. With
-    ///dimension checking enabled, sets the velocity and acceleration and returns [`Ok`] if the
-    ///argument's [`Unit`] is correct, otherwise leaves them unchanged and returns [`Err`]. With
-    ///dimension checking disabled, ignores the [`Unit`] and always sets velocity and acceleration
-    ///and returns [`Ok`].
-    pub const fn set_constant_velocity(
-        &mut self,
-        velocity: Quantity,
-    ) -> Result<(), error::UnitInvalid> {
-        if velocity.unit.eq_assume_true(&MILLIMETER_PER_SECOND) {
-            self.acceleration = 0.0;
-            self.velocity = velocity.value;
-            Ok(())
-        } else {
-            Err(error::UnitInvalid)
-        }
-    }*/
+    ///Set the velocity to a given value and set the acceleration to zero.
+    #[inline]
+    pub const fn set_constant_velocity(&mut self, velocity: MillimeterPerSecond<f32>) {
+        self.acceleration = MillimeterPerSecondSquared::new(0.0);
+        self.velocity = velocity;
+    }
     ///Set the velocity to a given value with an [`f32`] of millimeters per second, and set acceleration to zero.
     #[inline]
     pub const fn set_constant_velocity_raw(&mut self, velocity: f32) {
         self.acceleration = MillimeterPerSecondSquared::new(0.0);
         self.velocity = MillimeterPerSecond::new(velocity);
     }
-    /*///Set the position to a given value with a [`Quantity`], and set velocity and acceleration to
-    ///zero. With dimension checking enabled, sets the position, velocity, and acceleration and
-    ///returns [`Ok`] if the argument's [`Unit`] is correct, otherwise leaves them unchanged and
-    ///returns [`Err`]. With dimension checking disabled, always sets the position, velocity, and
-    ///acceleration and returns [`Ok`], ignoring the [`Unit`].
-    pub const fn set_constant_position(
-        &mut self,
-        position: Quantity,
-    ) -> Result<(), error::UnitInvalid> {
-        if position.unit.eq_assume_true(&MILLIMETER) {
-            self.acceleration = 0.0;
-            self.velocity = 0.0;
-            self.position = position.value;
-            Ok(())
-        } else {
-            Err(error::UnitInvalid)
-        }
-    }*/
+    ///Set the position to a given value and set the velocity and acceleration to zero.
+    #[inline]
+    pub const fn set_constant_position(&mut self, position: Millimeter<f32>) {
+        self.acceleration = MillimeterPerSecondSquared::new(0.0);
+        self.velocity = MillimeterPerSecond::new(0.0);
+        self.position = position;
+    }
     ///Set the position to a given value with an [`f32`] of millimeters, and set velocity and acceleration to zero.
     #[inline]
     pub const fn set_constant_position_raw(&mut self, position: f32) {
