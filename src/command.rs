563ae187672d3480c293daeb54bb3569b7e78396
Uxugin <git@uxugin.com>
2025-06-21 12:07:05 -0500

    parametrize Command enum making macro

--- src/command.rs
+++ src/command.rs
@@ -5,16 +5,16 @@ macro_rules! build_command_enum {
     ($name: ident, $pos: ty, $vel: ty, $acc: ty) => {
         ///A command for a motor to perform: go to a position, run at a velocity, or accelerate at a rate.
         #[derive(Clone, Copy, Debug, PartialEq)]
-        pub enum Command {
+        pub enum $name {
             ///Where you want to be. This should be in millimeters.
-            Position(Millimeter<f32>),
+            Position($pos),
             ///How fast you want to be going. This should be in millimeters per second.
-            Velocity(MillimeterPerSecond<f32>),
+            Velocity($vel),
             ///How fast you want how fast you're going to change. This should be in millimeters per second squared.
-            Acceleration(MillimeterPerSecondSquared<f32>),
+            Acceleration($acc),
         }
-        impl Command {
-            ///Constructor for [`Command`].
+        impl $name {
+            ///Constructor for [`$name`].
             pub const fn new(position_derivative: PositionDerivative, value: f32) -> Self {
                 match position_derivative {
                     PositionDerivative::Position => Self::Position(Millimeter::new(value)),
@@ -26,7 +26,7 @@ macro_rules! build_command_enum {
             }
             ///Get the commanded constant position if there is one. If the position derivative is
             ///velocity or acceleration, this will return `None` as there is not a constant position.
-            pub const fn get_position(&self) -> Option<Millimeter<f32>> {
+            pub const fn get_position(&self) -> Option<$pos> {
                 if let Self::Position(pos) = self {
                     Some(*pos)
                 } else {
@@ -37,7 +37,7 @@ macro_rules! build_command_enum {
             ///acceleration, this will return `None` as there is not a constant
             ///velocity. If the position derivative is position, this will return 0 as
             ///velocity should be zero with a constant position.
-            pub const fn get_velocity(&self) -> Option<MillimeterPerSecond<f32>> {
+            pub const fn get_velocity(&self) -> Option<$vel> {
                 match self {
                     Self::Position(_) => Some(MillimeterPerSecond::new(0.0)),
                     Self::Velocity(vel) => Some(*vel),
@@ -47,7 +47,7 @@ macro_rules! build_command_enum {
             ///Get the commanded constant acceleration. If the position derivative is not
             ///acceleration, this will return 0 as acceleration should be zero with a constant velocity or
             ///position.
-            pub const fn get_acceleration(&self) -> MillimeterPerSecondSquared<f32> {
+            pub const fn get_acceleration(&self) -> $acc {
                 if let Self::Acceleration(acc) = self {
                     *acc
                 } else {
@@ -55,25 +55,25 @@ macro_rules! build_command_enum {
                 }
             }
         }
-        impl From<Millimeter<f32>> for Command {
-            fn from(was: Millimeter<f32>) -> Self {
+        impl From<$pos> for $name {
+            fn from(was: $pos) -> Self {
                 Self::Position(was)
             }
         }
-        impl From<MillimeterPerSecond<f32>> for Command {
-            fn from(was: MillimeterPerSecond<f32>) -> Self {
+        impl From<$vel> for $name {
+            fn from(was: $vel) -> Self {
                 Self::Velocity(was)
             }
         }
-        impl From<MillimeterPerSecondSquared<f32>> for Command {
-            fn from(was: MillimeterPerSecondSquared<f32>) -> Self {
+        impl From<$acc> for $name {
+            fn from(was: $acc) -> Self {
                 Self::Acceleration(was)
             }
         }
-        impl From<State> for Command {
+        impl From<State> for $name {
             fn from(state: State) -> Self {
-                if state.acceleration == MillimeterPerSecondSquared::new(0.0) {
-                    if state.velocity == MillimeterPerSecond::new(0.0) {
+                if state.acceleration == <$acc>::new(0.0) {
+                    if state.velocity == <$vel>::new(0.0) {
                         Self::Position(state.position)
                     } else {
                         Self::Velocity(state.velocity)
@@ -83,16 +83,16 @@ macro_rules! build_command_enum {
                 }
             }
         }
-        impl From<Command> for f32 {
-            fn from(was: Command) -> f32 {
+        impl From<$name> for f32 {
+            fn from(was: $name) -> f32 {
                 match was {
-                    Command::Position(pos) => pos.into_inner(),
-                    Command::Velocity(vel) => vel.into_inner(),
-                    Command::Acceleration(acc) => acc.into_inner(),
+                    $name::Position(pos) => pos.into_inner(),
+                    $name::Velocity(vel) => vel.into_inner(),
+                    $name::Acceleration(acc) => acc.into_inner(),
                 }
             }
         }
-        impl Add for Command {
+        impl Add for $name {
             type Output = Self;
             fn add(self, rhs: Self) -> Self {
                 let self_pos_der = PositionDerivative::from(self);
@@ -100,7 +100,7 @@ macro_rules! build_command_enum {
                 Self::new(self_pos_der, f32::from(self) + f32::from(rhs))
             }
         }
-        impl Sub for Command {
+        impl Sub for $name {
             type Output = Self;
             fn sub(self, rhs: Self) -> Self {
                 let self_pos_der = PositionDerivative::from(self);
@@ -108,7 +108,7 @@ macro_rules! build_command_enum {
                 Self::new(self_pos_der, f32::from(self) - f32::from(rhs))
             }
         }
-        impl Mul<Dimensionless<f32>> for Command {
+        impl Mul<Dimensionless<f32>> for $name {
             type Output = Self;
             fn mul(self, rhs: Dimensionless<f32>) -> Self {
                 match self {
@@ -118,7 +118,7 @@ macro_rules! build_command_enum {
                 }
             }
         }
-        impl Div<Dimensionless<f32>> for Command {
+        impl Div<Dimensionless<f32>> for $name {
             type Output = Self;
             fn div(self, rhs: Dimensionless<f32>) -> Self {
                 match self {
@@ -128,7 +128,7 @@ macro_rules! build_command_enum {
                 }
             }
         }
-        impl Neg for Command {
+        impl Neg for $name {
             type Output = Self;
             fn neg(self) -> Self {
                 match self {
@@ -138,19 +138,19 @@ macro_rules! build_command_enum {
                 }
             }
         }
-        impl AddAssign for Command {
+        impl AddAssign for $name {
             fn add_assign(&mut self, rhs: Self) {
                 *self = *self + rhs;
             }
         }
-        impl SubAssign for Command {
+        impl SubAssign for $name {
             fn sub_assign(&mut self, rhs: Self) {
                 *self = *self - rhs;
             }
         }
         //You might be able to optimize this a bit more with an unsafe dereference of the field since it's
         //always an f32 and the variant never changes.
-        impl MulAssign<Dimensionless<f32>> for Command {
+        impl MulAssign<Dimensionless<f32>> for $name {
             fn mul_assign(&mut self, rhs: Dimensionless<f32>) {
                 match self {
                     Self::Position(pos) => *pos *= rhs,
@@ -159,7 +159,7 @@ macro_rules! build_command_enum {
                 }
             }
         }
-        impl DivAssign<Dimensionless<f32>> for Command {
+        impl DivAssign<Dimensionless<f32>> for $name {
             fn div_assign(&mut self, rhs: Dimensionless<f32>) {
                 match self {
                     Self::Position(pos) => *pos /= rhs,
