452f587ccb31b4386fa35ecec93252aea7cd1017
Uxugin <git@uxugin.com>
2025-07-14 06:17:51 -0400

    make connect_terminals actually work

--- src/new_devices.rs
+++ src/new_devices.rs
@@ -21,12 +21,14 @@ struct IIdentifyAsAVec<const N: usize> {
     length: usize,
 }
 impl<const N: usize> IIdentifyAsAVec<N> {
+    #[inline]
     const fn new() -> Self {
         Self {
             inner: [MaybeUninit::uninit(); N],
             length: 0,
         }
     }
+    #[inline]
     const fn push(&mut self, id: TerminalID) {
         if self.length >= N {
             panic!("You overflowed an IIdentifyAsAVec.");
@@ -34,12 +36,14 @@ impl<const N: usize> IIdentifyAsAVec<N> {
         self.inner[self.length].write(id);
         self.length += 1;
     }
+    #[inline]
     const fn get(&self, index: usize) -> TerminalID {
         if index >= self.length {
             panic!("This index is out of range.");
         }
         unsafe { self.inner[index].assume_init() }
     }
+    #[inline]
     const fn pop(&mut self) -> TerminalID {
         if self.length == 0 {
             panic!("You tried to pop from an empty IIdentifyAsAVec.");
@@ -48,6 +52,7 @@ impl<const N: usize> IIdentifyAsAVec<N> {
         self.length -= 1;
         output
     }
+    #[inline]
     const fn as_array(&self) -> [TerminalID; N] {
         if self.length != N {
             panic!("You tried to convert a non-full IIdentifyAsAVec to an array.");
@@ -61,6 +66,10 @@ impl<const N: usize> IIdentifyAsAVec<N> {
         const_for!(i, 0, self.length, {
             system.release_terminal(self.get(i));
         });
+    }
+    #[inline]
+    const fn len(&self) -> usize {
+        self.length
     }
 }
 #[derive(Clone, Copy, Default, PartialEq)]
@@ -146,10 +155,20 @@ impl<const N: usize> System<N> {
     pub const fn connect_terminals(&mut self, id_a: TerminalID, id_b: TerminalID) {
         self.verify_terminal_id(id_a);
         self.verify_terminal_id(id_b);
-        if id_a.terminal > id_b.terminal {
-            self.terminals[id_a.terminal] = MaybeTerminal::Connected(id_b.terminal);
+        let a_connected = self.get_connected(id_a);
+        let b_connected = self.get_connected(id_b);
+        let a_root = a_connected.get(0);
+        let b_root = b_connected.get(0);
+        if a_root.terminal > b_root.terminal {
+            const_for!(i, 0, a_connected.len(), {
+                self.terminals[a_connected.get(i).terminal] =
+                    MaybeTerminal::Connected(b_root.terminal);
+            });
         } else if id_b.terminal > id_a.terminal {
-            self.terminals[id_b.terminal] = MaybeTerminal::Connected(id_a.terminal);
+            const_for!(i, 0, b_connected.len(), {
+                self.terminals[b_connected.get(i).terminal] =
+                    MaybeTerminal::Connected(b_root.terminal);
+            });
         }
     }
     const fn get_root(&self, id: TerminalID) -> usize {
