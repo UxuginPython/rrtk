acf7172e0e350843887743e01edd1a92523f2aa9
Uxugin <git@uxugin.com>
2025-07-16 09:52:33 -0400

    fix example

--- examples/new_devices.rs
+++ examples/new_devices.rs
@@ -5,16 +5,19 @@ fn display(system: &System<3>, differential: &Differential) {
         "{:?} + {:?} = {:?}",
         system
             .get_terminal_state(differential.side_a)
+            .unwrap()
             .value
             .velocity
             .into_inner(),
         system
             .get_terminal_state(differential.side_b)
+            .unwrap()
             .value
             .velocity
             .into_inner(),
         system
             .get_terminal_state(differential.sum_side)
+            .unwrap()
             .value
             .velocity
             .into_inner()
@@ -23,6 +26,18 @@ fn display(system: &System<3>, differential: &Differential) {
 fn main() {
     let mut system = System::<3>::new();
     let mut differential = Differential::new(&mut system).unwrap();
+    system.set_terminal_state(
+        differential.side_a,
+        Datum::new(Time::ZERO, AngularState::ZERO),
+    );
+    system.set_terminal_state(
+        differential.side_b,
+        Datum::new(Time::ZERO, AngularState::ZERO),
+    );
+    system.set_terminal_state(
+        differential.sum_side,
+        Datum::new(Time::ZERO, AngularState::ZERO),
+    );
     display(&system, &differential);
     system.set_terminal_state(
         differential.side_a,
