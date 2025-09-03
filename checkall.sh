6f6135fb67a14242ed4b74ab9832cf8caf430ba7
Uxugin <git@uxugin.com>
2025-09-03 16:49:18 -0600

    prepare for release

--- checkall.sh
+++ checkall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated automatically by rrtk 0.7.0-alpha.8
+#Generated automatically by rrtk 0.7.0-alpha.9
 set -e
 echo
 cargo check --no-default-features
@@ -9,139 +9,91 @@ echo std
 cargo check --no-default-features --features std
 echo devices
 cargo check --no-default-features --features devices
-echo dim_check_debug
-cargo check --no-default-features --features dim_check_debug
-echo dim_check_release
-cargo check --no-default-features --features dim_check_release
+echo new_devices
+cargo check --no-default-features --features new_devices
 echo libm
 cargo check --no-default-features --features libm
 echo micromath
 cargo check --no-default-features --features micromath
 echo alloc devices
 cargo check --no-default-features --features alloc,devices
-echo alloc dim_check_debug
-cargo check --no-default-features --features alloc,dim_check_debug
-echo alloc dim_check_release
-cargo check --no-default-features --features alloc,dim_check_release
+echo alloc new_devices
+cargo check --no-default-features --features alloc,new_devices
 echo alloc libm
 cargo check --no-default-features --features alloc,libm
 echo alloc micromath
 cargo check --no-default-features --features alloc,micromath
 echo std devices
 cargo check --no-default-features --features std,devices
-echo std dim_check_debug
-cargo check --no-default-features --features std,dim_check_debug
-echo std dim_check_release
-cargo check --no-default-features --features std,dim_check_release
+echo std new_devices
+cargo check --no-default-features --features std,new_devices
 echo std libm
 cargo check --no-default-features --features std,libm
 echo std micromath
 cargo check --no-default-features --features std,micromath
-echo devices dim_check_debug
-cargo check --no-default-features --features devices,dim_check_debug
-echo devices dim_check_release
-cargo check --no-default-features --features devices,dim_check_release
+echo devices new_devices
+cargo check --no-default-features --features devices,new_devices
 echo devices libm
 cargo check --no-default-features --features devices,libm
 echo devices micromath
 cargo check --no-default-features --features devices,micromath
-echo dim_check_debug libm
-cargo check --no-default-features --features dim_check_debug,libm
-echo dim_check_debug micromath
-cargo check --no-default-features --features dim_check_debug,micromath
-echo dim_check_release libm
-cargo check --no-default-features --features dim_check_release,libm
-echo dim_check_release micromath
-cargo check --no-default-features --features dim_check_release,micromath
+echo new_devices libm
+cargo check --no-default-features --features new_devices,libm
+echo new_devices micromath
+cargo check --no-default-features --features new_devices,micromath
 echo libm micromath
 cargo check --no-default-features --features libm,micromath
-echo alloc devices dim_check_debug
-cargo check --no-default-features --features alloc,devices,dim_check_debug
-echo alloc devices dim_check_release
-cargo check --no-default-features --features alloc,devices,dim_check_release
+echo alloc devices new_devices
+cargo check --no-default-features --features alloc,devices,new_devices
 echo alloc devices libm
 cargo check --no-default-features --features alloc,devices,libm
 echo alloc devices micromath
 cargo check --no-default-features --features alloc,devices,micromath
-echo alloc dim_check_debug libm
-cargo check --no-default-features --features alloc,dim_check_debug,libm
-echo alloc dim_check_debug micromath
-cargo check --no-default-features --features alloc,dim_check_debug,micromath
-echo alloc dim_check_release libm
-cargo check --no-default-features --features alloc,dim_check_release,libm
-echo alloc dim_check_release micromath
-cargo check --no-default-features --features alloc,dim_check_release,micromath
+echo alloc new_devices libm
+cargo check --no-default-features --features alloc,new_devices,libm
+echo alloc new_devices micromath
+cargo check --no-default-features --features alloc,new_devices,micromath
 echo alloc libm micromath
 cargo check --no-default-features --features alloc,libm,micromath
-echo std devices dim_check_debug
-cargo check --no-default-features --features std,devices,dim_check_debug
-echo std devices dim_check_release
-cargo check --no-default-features --features std,devices,dim_check_release
+echo std devices new_devices
+cargo check --no-default-features --features std,devices,new_devices
 echo std devices libm
 cargo check --no-default-features --features std,devices,libm
 echo std devices micromath
 cargo check --no-default-features --features std,devices,micromath
-echo std dim_check_debug libm
-cargo check --no-default-features --features std,dim_check_debug,libm
-echo std dim_check_debug micromath
-cargo check --no-default-features --features std,dim_check_debug,micromath
-echo std dim_check_release libm
-cargo check --no-default-features --features std,dim_check_release,libm
-echo std dim_check_release micromath
-cargo check --no-default-features --features std,dim_check_release,micromath
+echo std new_devices libm
+cargo check --no-default-features --features std,new_devices,libm
+echo std new_devices micromath
+cargo check --no-default-features --features std,new_devices,micromath
 echo std libm micromath
 cargo check --no-default-features --features std,libm,micromath
-echo devices dim_check_debug libm
-cargo check --no-default-features --features devices,dim_check_debug,libm
-echo devices dim_check_debug micromath
-cargo check --no-default-features --features devices,dim_check_debug,micromath
-echo devices dim_check_release libm
-cargo check --no-default-features --features devices,dim_check_release,libm
-echo devices dim_check_release micromath
-cargo check --no-default-features --features devices,dim_check_release,micromath
+echo devices new_devices libm
+cargo check --no-default-features --features devices,new_devices,libm
+echo devices new_devices micromath
+cargo check --no-default-features --features devices,new_devices,micromath
 echo devices libm micromath
 cargo check --no-default-features --features devices,libm,micromath
-echo dim_check_debug libm micromath
-cargo check --no-default-features --features dim_check_debug,libm,micromath
-echo dim_check_release libm micromath
-cargo check --no-default-features --features dim_check_release,libm,micromath
-echo alloc devices dim_check_debug libm
-cargo check --no-default-features --features alloc,devices,dim_check_debug,libm
-echo alloc devices dim_check_debug micromath
-cargo check --no-default-features --features alloc,devices,dim_check_debug,micromath
-echo alloc devices dim_check_release libm
-cargo check --no-default-features --features alloc,devices,dim_check_release,libm
-echo alloc devices dim_check_release micromath
-cargo check --no-default-features --features alloc,devices,dim_check_release,micromath
+echo new_devices libm micromath
+cargo check --no-default-features --features new_devices,libm,micromath
+echo alloc devices new_devices libm
+cargo check --no-default-features --features alloc,devices,new_devices,libm
+echo alloc devices new_devices micromath
+cargo check --no-default-features --features alloc,devices,new_devices,micromath
 echo alloc devices libm micromath
 cargo check --no-default-features --features alloc,devices,libm,micromath
-echo alloc dim_check_debug libm micromath
-cargo check --no-default-features --features alloc,dim_check_debug,libm,micromath
-echo alloc dim_check_release libm micromath
-cargo check --no-default-features --features alloc,dim_check_release,libm,micromath
-echo std devices dim_check_debug libm
-cargo check --no-default-features --features std,devices,dim_check_debug,libm
-echo std devices dim_check_debug micromath
-cargo check --no-default-features --features std,devices,dim_check_debug,micromath
-echo std devices dim_check_release libm
-cargo check --no-default-features --features std,devices,dim_check_release,libm
-echo std devices dim_check_release micromath
-cargo check --no-default-features --features std,devices,dim_check_release,micromath
+echo alloc new_devices libm micromath
+cargo check --no-default-features --features alloc,new_devices,libm,micromath
+echo std devices new_devices libm
+cargo check --no-default-features --features std,devices,new_devices,libm
+echo std devices new_devices micromath
+cargo check --no-default-features --features std,devices,new_devices,micromath
 echo std devices libm micromath
 cargo check --no-default-features --features std,devices,libm,micromath
-echo std dim_check_debug libm micromath
-cargo check --no-default-features --features std,dim_check_debug,libm,micromath
-echo std dim_check_release libm micromath
-cargo check --no-default-features --features std,dim_check_release,libm,micromath
-echo devices dim_check_debug libm micromath
-cargo check --no-default-features --features devices,dim_check_debug,libm,micromath
-echo devices dim_check_release libm micromath
-cargo check --no-default-features --features devices,dim_check_release,libm,micromath
-echo alloc devices dim_check_debug libm micromath
-cargo check --no-default-features --features alloc,devices,dim_check_debug,libm,micromath
-echo alloc devices dim_check_release libm micromath
-cargo check --no-default-features --features alloc,devices,dim_check_release,libm,micromath
-echo std devices dim_check_debug libm micromath
-cargo check --no-default-features --features std,devices,dim_check_debug,libm,micromath
-echo std devices dim_check_release libm micromath
-cargo check --no-default-features --features std,devices,dim_check_release,libm,micromath
+echo std new_devices libm micromath
+cargo check --no-default-features --features std,new_devices,libm,micromath
+echo devices new_devices libm micromath
+cargo check --no-default-features --features devices,new_devices,libm,micromath
+echo alloc devices new_devices libm micromath
+cargo check --no-default-features --features alloc,devices,new_devices,libm,micromath
+echo std devices new_devices libm micromath
+cargo check --no-default-features --features std,devices,new_devices,libm,micromath
