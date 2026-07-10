eb3f91775155dd80f422db5ad913dd325abbed28
Uxugin <git@uxugin.com>
2026-07-10 06:40:25 -0600

    use the new macros in the tests

--- tests/dimensionless_fraction.rs
+++ tests/dimensionless_fraction.rs
@@ -30,56 +30,50 @@ fn div_by_zero_constructor_validation_macro() {
 }
 #[test]
 fn is_valid() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(-1), DimensionlessInteger(2));
+    let x = dimensionless_fraction!(-1, 2);
     assert!(x.is_valid());
-    let y = DimensionlessFraction::new_unchecked(DimensionlessInteger(-1), DimensionlessInteger(0));
+    let y = dimensionless_fraction_unchecked!(-1, 0);
     assert!(!y.is_valid());
 }
 #[test]
 fn reciprocal() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(2), DimensionlessInteger(-3));
-    assert_eq!(
-        x.reciprocal(),
-        DimensionlessFraction::new(DimensionlessInteger(-3), DimensionlessInteger(2))
-    );
+    let x = dimensionless_fraction!(2, -3);
+    assert_eq!(x.reciprocal(), dimensionless_fraction!(-3, 2),);
 }
 #[test]
 #[should_panic]
 fn reciprocal_div_by_zero() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(0), DimensionlessInteger(-5));
+    let x = dimensionless_fraction!(0, -5);
     let _ = x.reciprocal();
 }
 #[test]
 fn reciprocal_unchecked() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(2), DimensionlessInteger(-3));
-    assert_eq!(
-        x.reciprocal_unchecked(),
-        DimensionlessFraction::new(DimensionlessInteger(-3), DimensionlessInteger(2))
-    );
-    let y = DimensionlessFraction::new(DimensionlessInteger(0), DimensionlessInteger(-3));
+    let x = dimensionless_fraction!(2, -3);
+    assert_eq!(x.reciprocal_unchecked(), dimensionless_fraction!(-3, 2),);
+    let y = dimensionless_fraction!(0, -3);
     assert_eq!(
         y.reciprocal_unchecked(),
-        DimensionlessFraction::new_unchecked(DimensionlessInteger(-3), DimensionlessInteger(0))
+        dimensionless_fraction_unchecked!(-3, 0),
     );
 }
 #[test]
 fn as_f32() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(3), DimensionlessInteger(2));
+    let x = dimensionless_fraction!(3, 2);
     assert_eq!(x.as_f32(), 1.5f32);
 }
 #[test]
 fn as_f64() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(3), DimensionlessInteger(2));
+    let x = dimensionless_fraction!(3, 2);
     assert_eq!(x.as_f64(), 1.5f64);
 }
 #[test]
 fn as_quantity_f32() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(3), DimensionlessInteger(2));
+    let x = dimensionless_fraction!(3, 2);
     assert_eq!(x.as_quantity_f32(), Dimensionless::new(1.5f32));
 }
 #[test]
 fn as_quantity_f64() {
-    let x = DimensionlessFraction::new(DimensionlessInteger(3), DimensionlessInteger(2));
+    let x = dimensionless_fraction!(3, 2);
     assert_eq!(x.as_quantity_f64(), Dimensionless::new(1.5f64));
 }
 #[test]
