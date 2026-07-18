6294988227a99a2837a66a6764c91e4f7922509e
Uxugin <git@uxugin.com>
2026-07-18 13:22:13 -0400

    one final Feeder test

--- tests/feeder.rs
+++ tests/feeder.rs
@@ -208,3 +208,47 @@ fn settable_set_fail() {
     assert_eq!(unsafe { MY_GETTER_UPDATE_CALL_COUNT }, 1);
     assert_eq!(unsafe { MY_GETTER_GET_CALL_COUNT }, 1);
 }
+#[test]
+fn getter_ok_settable_update_fail() {
+    struct MyGetter;
+    static mut MY_GETTER_UPDATE_CALL_COUNT: u8 = 0;
+    impl Updatable<u8> for MyGetter {
+        fn update(&mut self) -> NothingOrError<u8> {
+            unsafe {
+                MY_GETTER_UPDATE_CALL_COUNT += 1;
+            }
+            Ok(())
+        }
+    }
+    static mut MY_GETTER_GET_CALL_COUNT: u8 = 0;
+    impl Getter<u8, u8> for MyGetter {
+        fn get(&self) -> Output<u8, u8> {
+            unsafe {
+                MY_GETTER_GET_CALL_COUNT += 1;
+            }
+            CORRECT_VALUE
+        }
+    }
+    struct MySettable;
+    static mut MY_SETTABLE_SET_CALL_COUNT: u8 = 0;
+    impl Settable<u8, u8> for MySettable {
+        fn set(&mut self, value: u8) -> NothingOrError<u8> {
+            assert_eq!(value, 39);
+            unsafe {
+                MY_SETTABLE_SET_CALL_COUNT += 1;
+            }
+            Ok(())
+        }
+    }
+    impl Updatable<u8> for MySettable {
+        fn update(&mut self) -> NothingOrError<u8> {
+            Err(4)
+        }
+    }
+    let mut feeder = Feeder::new(MyGetter, MySettable);
+    let test = feeder.update();
+    assert_eq!(test, Err(error::PossibleDoubleError::B(4)));
+    assert_eq!(unsafe { MY_GETTER_UPDATE_CALL_COUNT }, 1);
+    assert_eq!(unsafe { MY_GETTER_GET_CALL_COUNT }, 1);
+    assert_eq!(unsafe { MY_SETTABLE_SET_CALL_COUNT }, 1);
+}
