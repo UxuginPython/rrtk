b3ab216571d6871277f01385efc03e879f5b4e5b
Uxugin <git@uxugin.com>
2026-07-26 09:02:04 -0400

    more of that

--- src/devices/wrappers.rs
+++ src/devices/wrappers.rs
@@ -18,3 +18,18 @@ pub fn get_and_write_to_node<
     system.set_state_local(node, to_set);
     Ok(())
 }
+pub fn set_to_node_state<
+    S: Settable<AngularState, E> + ?Sized,
+    const N: usize,
+    E: Clone + Debug,
+>(
+    settable: &mut S,
+    system: &mut System<N>,
+    node: NodeID,
+) -> NothingOrError<E> {
+    if let Some(state) = system.get_state_connected(node) {
+        settable.set(state)
+    } else {
+        Ok(())
+    }
+}
