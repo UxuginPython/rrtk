6b33448c4dbe21ed04639333f32c9c39593d5a96
Uxugin <git@uxugin.com>
2026-08-18 22:55:05 -0600

    correctly require the alloc feature for the pid2 example; add the copyright notice

--- examples/pid2.rs
+++ examples/pid2.rs
@@ -1,64 +1,79 @@
-extern crate alloc;
-use alloc::rc::Rc;
-use core::{cell::RefCell, convert::Infallible};
-use rrtk::prelude::*;
-use rrtk::streams::{converters, math};
-use rrtk::{ConstantGetter, NothingOrError, Output, Time};
-struct Input {
-    time: Time,
-}
-impl Updatable<Infallible> for Input {
-    fn update(&mut self) -> NothingOrError<Infallible> {
-        self.time += Time::from_nanoseconds(200_000_000);
-        Ok(())
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright 2024-2026 UxuginPython
+#[cfg(feature = "alloc")]
+mod example {
+    extern crate alloc;
+    use alloc::rc::Rc;
+    use core::{cell::RefCell, convert::Infallible};
+    use rrtk::prelude::*;
+    use rrtk::streams::{converters, math};
+    use rrtk::{ConstantGetter, NothingOrError, Output, Time};
+    struct Input {
+        time: Time,
     }
-}
-static mut CONTROLLED_VALUE: f32 = 0.0;
-impl Getter<f32, Infallible> for Input {
-    fn get(&self) -> Output<f32, Infallible> {
-        Ok(Some(Datum::new(self.time, unsafe { CONTROLLED_VALUE })))
-    }
-}
-fn main() {
-    const SETPOINT: f32 = 5.0;
-    const KP: f32 = 1.0;
-    const KI: f32 = 0.01;
-    const KD: f32 = 0.1;
-    let input = Input {
-        time: Time::from_nanoseconds(0),
-    };
-    const FAKE_TIME_GETTER: Time = Time::from_nanoseconds(i64::MIN);
-    let setpoint = ConstantGetter::new(FAKE_TIME_GETTER, SETPOINT);
-    let error = Rc::new(RefCell::new(math::DifferenceStream::new(setpoint, input)));
-    let kp = ConstantGetter::new(FAKE_TIME_GETTER, KP);
-    let proportional_term =
-        Box::new(math::Product2::new(Rc::clone(&error), kp)) as Box<dyn Getter<f32, Infallible>>;
-    let integral = converters::PrioritizeA::new(converters::NoneToDefault::new(
-        math::IntegralStream::new(Rc::clone(&error)),
-        FAKE_TIME_GETTER,
-    ));
-    let ki = ConstantGetter::new(FAKE_TIME_GETTER, KI);
-    let integral_term =
-        Box::new(math::Product2::new(integral, ki)) as Box<dyn Getter<f32, Infallible>>;
-    let derivative = converters::PrioritizeA::new(converters::NoneToDefault::new(
-        math::DerivativeStream::new(Rc::clone(&error)),
-        FAKE_TIME_GETTER,
-    ));
-    let kd = ConstantGetter::new(FAKE_TIME_GETTER, KD);
-    let derivative_term =
-        Box::new(math::Product2::new(derivative, kd)) as Box<dyn Getter<f32, Infallible>>;
-    let mut pid = math::SumStream::new([proportional_term, integral_term, derivative_term]);
-    for _ in 0..30 {
-        pid.update().unwrap();
-        let gotten = pid.get().unwrap().unwrap();
-        println!(
-            "time: {:?};\tcontrolled value: {:?};\tcommand: {:?}",
-            gotten.time.as_nanoseconds(),
-            unsafe { CONTROLLED_VALUE },
-            gotten.value
-        );
-        unsafe {
-            CONTROLLED_VALUE += 0.7 * gotten.value;
+    impl Updatable<Infallible> for Input {
+        fn update(&mut self) -> NothingOrError<Infallible> {
+            self.time += Time::from_nanoseconds(200_000_000);
+            Ok(())
         }
     }
+    static mut CONTROLLED_VALUE: f32 = 0.0;
+    impl Getter<f32, Infallible> for Input {
+        fn get(&self) -> Output<f32, Infallible> {
+            Ok(Some(Datum::new(self.time, unsafe { CONTROLLED_VALUE })))
+        }
+    }
+    pub fn main() {
+        const SETPOINT: f32 = 5.0;
+        const KP: f32 = 1.0;
+        const KI: f32 = 0.01;
+        const KD: f32 = 0.1;
+        let input = Input {
+            time: Time::from_nanoseconds(0),
+        };
+        const FAKE_TIME_GETTER: Time = Time::from_nanoseconds(i64::MIN);
+        let setpoint = ConstantGetter::new(FAKE_TIME_GETTER, SETPOINT);
+        let error = Rc::new(RefCell::new(math::DifferenceStream::new(setpoint, input)));
+        let kp = ConstantGetter::new(FAKE_TIME_GETTER, KP);
+        let proportional_term = Box::new(math::Product2::new(Rc::clone(&error), kp))
+            as Box<dyn Getter<f32, Infallible>>;
+        let integral = converters::PrioritizeA::new(converters::NoneToDefault::new(
+            math::IntegralStream::new(Rc::clone(&error)),
+            FAKE_TIME_GETTER,
+        ));
+        let ki = ConstantGetter::new(FAKE_TIME_GETTER, KI);
+        let integral_term =
+            Box::new(math::Product2::new(integral, ki)) as Box<dyn Getter<f32, Infallible>>;
+        let derivative = converters::PrioritizeA::new(converters::NoneToDefault::new(
+            math::DerivativeStream::new(Rc::clone(&error)),
+            FAKE_TIME_GETTER,
+        ));
+        let kd = ConstantGetter::new(FAKE_TIME_GETTER, KD);
+        let derivative_term =
+            Box::new(math::Product2::new(derivative, kd)) as Box<dyn Getter<f32, Infallible>>;
+        let mut pid = math::SumStream::new([proportional_term, integral_term, derivative_term]);
+        for _ in 0..30 {
+            pid.update().unwrap();
+            let gotten = pid.get().unwrap().unwrap();
+            println!(
+                "time: {:?};\tcontrolled value: {:?};\tcommand: {:?}",
+                gotten.time.as_nanoseconds(),
+                unsafe { CONTROLLED_VALUE },
+                gotten.value
+            );
+            unsafe {
+                CONTROLLED_VALUE += 0.7 * gotten.value;
+            }
+        }
+    }
+}
+#[cfg(feature = "alloc")]
+fn main() {
+    example::main();
+}
+#[cfg(not(feature = "alloc"))]
+fn main() {
+    eprintln!(
+        "Enable the `alloc` feature to run this example.\nAssuming you're using Cargo, add `--features alloc` to your command."
+    );
 }
