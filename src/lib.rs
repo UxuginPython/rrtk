ead483463f61736c69e6ed2155fbcd2742bf1f18
Uxugin <git@uxugin.com>
2026-08-19 14:20:27 -0600

    more the missing_docs lint to Cargo.toml and add some extra Clippy lints

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,6 @@
 //!
 //!RRTK prefers **`std`** over **`libm`** and `libm` over **`micromath`** when multiple are
 //!available.
-#![warn(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #[cfg(all(
     feature = "internal_enhanced_float",
